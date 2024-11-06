823fa4944bd4213f49566a18a2a7b0ef2e561376
Loubna Gharbi <loulizoga@gmail.com>
2024-11-06 22:59:29 +0100

    Déplacement du contenu de backend à la racine

--- backend/middleware/controlEmail.js
+++
@@ -1,7 +0,0 @@
-const validator = require('validator')
-
-module.exports = (req, res, next) => {
-    const {email} = req.body;
-    if(validator.isEmail(email)) next() 
-    else res.status(400).json({error : `l'email ${email} est invalide`})
-}
